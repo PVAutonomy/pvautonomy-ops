5f25048f5e959365de471ce8a8c5447dd2f67682
PVAutonomy <guenter@pvautonomy.com>
2026-06-12 09:42:03 +0200

    [RELEASE] pvautonomy_ops v0.4.8

--- custom_components/pvautonomy_ops/const.py
+++ custom_components/pvautonomy_ops/const.py
@@ -7,7 +7,7 @@ All entity IDs defined here - NO hardcodes elsewhere.
 
 # Integration metadata
 DOMAIN = "pvautonomy_ops"
-VERSION = "0.4.7"
+VERSION = "0.4.8"
 CONTRACT_VERSION = "v1.0.0"
 
 # Update interval (seconds)
